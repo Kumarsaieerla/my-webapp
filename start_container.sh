f27102a03bbaa4b854a7e9feefa6a2fb359d70ef
Kumarsaieerla <117519958+Kumarsaieerla@users.noreply.github.com>
2025-01-31 20:02:14 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 docker pull saieerla/webapp-qa:my-webapp
-docker run -d -p 8080:8080 --name my-webapp saieerla/webapp-qa:my-webapp
+docker run -d -p 8080:8080 --name my-webapps saieerla/webapp-qa:my-webapp
