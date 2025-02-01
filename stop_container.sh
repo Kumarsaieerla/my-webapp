32c1056a77ae519d126ce01b2fc22f3dfefbe38e
Kumarsaieerla <117519958+Kumarsaieerla@users.noreply.github.com>
2025-02-01 13:05:17 +0530

    Update stop_container.sh

--- stop_container.sh
+++ stop_container.sh
@@ -1,6 +1,12 @@
 
 #!/bin/bash
 set -e
-docker stop $(docker ps -q)
-docker rm $(docker ps -a -q)
-docker rmi $(docker images -q) --force
+
+# Stop all running containers
+container_ids=$(docker ps -q)
+if [ -n "$container_ids" ]; then
+  docker stop $container_ids
+else
+  echo "No containers to stop."
+fi
+
